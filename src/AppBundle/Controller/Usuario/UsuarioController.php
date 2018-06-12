d72b0e768a30f850491f7bd5563df749612cde78
ivanmendoza2011@hotmail.com <ivanmendoza2011@hotmail.com>
2018-06-11 21:12:13 -0400

    Subiendo Repositorios

---
+++ src/AppBundle/Controller/Usuario/UsuarioController.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivanm
+ * Date: 6/11/2018
+ * Time: 6:32 PM
+ */
+
+namespace AppBundle\Controller\Usuario;
+
+use AppBundle\Entity\Usuario;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UsuarioController extends Controller
+{
+    /**
+     * @Route("/usuario", name="visita_usuario")
+     */
+     public function indexUsuario(Request $request)
+     {
+         /*
+          * Insertar Usuario
+         $em = $this->getDoctrine()->getManager();
+
+         $usuario = new Usuario();
+         $usuario->setNombre("Hector Ventura");
+         $usuario->setUsername("hectorventura@gmail.com");
+
+         $em->persist($usuario);
+
+         $em->flush();
+        */
+
+         /*Traer todos los usuarios*/
+         $usuarios=$this->getDoctrine()
+             ->getRepository(Usuario::class)
+             ->findAll();
+
+         /*
+         dump($usuarios);
+         die;
+        */
+
+         return $this->render('@App/Usuario/lista.html.twig',
+             [
+                 "usuarios" => $usuarios
+             ]
+         );
+     }
+
+    /**
+     * @Route("/usuario/{idUsuario}", name="informacion_usuario")
+     */
+    public function indexUsuarioInfo($idUsuario)
+    {
+        /*Deben tener el mismo nombre para el get, el parametro y la ruta*/
+        /*dump solo se usa en desarrollo*/
+        //dump("Estamos viendo el usuario: ".$idUsuario);
+        return $this->render('@App/Usuario/index.html.twig', [
+            'idUsuario'=>$idUsuario
+        ]);
+
+    }
+}
---
+++ src/AppBundle/Entity/Usuario.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Usuario
+ *
+ * @ORM\Table(name="usuario")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\UsuarioRepository")
+ */
+class Usuario
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255, unique=true)
+     */
+    private $username;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Usuario
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return Usuario
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+}
+
