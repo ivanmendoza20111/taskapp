69d3c22b001e385a3a0a8b125f5b15265cb50661
ivanmendoza2011@hotmail.com <ivanmendoza2011@hotmail.com>
2018-06-18 21:45:40 -0400

    Subiendo API RESTFUL

--- src/AppBundle/Controller/Usuario/UsuarioController.php
+++ src/AppBundle/Controller/Usuario/UsuarioController.php
@@ -9,9 +9,15 @@
 namespace AppBundle\Controller\Usuario;
 
 use AppBundle\Entity\Usuario;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class UsuarioController extends Controller
 {
@@ -72,5 +78,93 @@ class UsuarioController extends Controller
             'idUsuario'=>$idUsuario
         ]);
 
+    }
+
+    //Restful API
+
+    /**
+    * @Route("/rest/usuario/", name="buscar_usuarios")
+    * @Method("GET")
+    */
+    public function buscarUsuarios(Request $request)
+    {
+        return null;
+    }
+
+    /**
+     * @Route("/rest/usuario/{id}", name="buscar_usuario")
+     * @Method("GET")
+     * @ParamConverter("usuario", class="AppBundle:Usuario")
+     * @param Usuario $usuario
+     */
+    public function buscarUsuario($usuario)
+    {
+        $encoders=array(new JsonEncoder());
+        $normalizers=array(new ObjectNormalizer());
+        $serializer=new Serializer($normalizers,$encoders);
+
+        $jsonContent=json_decode($serializer->serialize($usuario,'json'),true);
+
+        return new JsonResponse($jsonContent);
+    }
+
+    /**
+    * @Route("/rest/usuario", name="guardar_usuario")
+    * @Method("POST")
+    */
+    public function guardarUsuario(Request $request)
+    {
+        $data=$request->getContent();
+        $data=(json_decode($data,true));
+
+        $usuario=new Usuario();
+        $usuario->setNombre($data["nombre"]);
+        $usuario->setUsername($data["Username"]);
+
+        $em=$this->getDoctrine()->getManager();
+        $em->persist($usuario);
+        $em->flush();
+
+        $encoders=array(new JsonEncoder());
+        $normalizers=array(new ObjectNormalizer());
+        $serializer=new Serializer($normalizers,$encoders);
+
+        $jsonContent=json_decode($serializer->serialize($usuario,'json'),true);
+
+        return new JsonResponse($jsonContent);
+        // sFormType
+        // return null;
+        // Investigar jms_serializer
+        // Servicios en Symfony
+        // Tipos de formulario formTypes
+        //
+    }
+
+    /**
+     * @Route("/rest/usuario/{id}", name="actualizar_usuario")
+     * @Method("PUT")
+     * @ParamConverter("usuario", class="AppBundle:Usuario")
+     * @param Request $request
+     * @param Usuario $usuario
+     *
+     */
+    public function actualizarUsuario(Request $request,$usuario)
+    {
+        $data=$request->getContent();
+        $data=(json_decode($data,true));
+
+        $usuario->setNombre($data["nombre"]);
+        $usuario->setUsername($data["Username"]);
+
+        $encoders=array(new JsonEncoder());
+        $normalizers=array(new ObjectNormalizer());
+        $serializer=new Serializer($normalizers,$encoders);
+
+        $em=$this->getDoctrine()->getManager();
+        $em->flush();
+
+        $jsonContent=json_decode($serializer->serialize($usuario,'json'),true);
+
+        return new JsonResponse($jsonContent);;
     }
 }
